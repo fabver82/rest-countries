9419d87bff5dd3fd47572c8fab29921076e093b3
Fabien V <fabien.vermeiren@gmail.com>
2022-03-18 16:37:12 +0100

    js getcountry + add img in template

---
+++ public/js/countries.js
@@ -0,0 +1,27 @@
+const getAllCountries = async ()=>{
+    const reponse = await fetch('https://restcountries.com/v2/all');
+    const datas = await reponse.json();
+    console.log(datas);
+}
+const getCountryByName = async (name)=>{
+    const reponse = await fetch(`https://restcountries.com/v2/name/${name}`);
+    const datas = await reponse.json();
+    return datas;
+}
+const countriesHtml = document.getElementById('countries');
+const fillDatas = async ()=>{
+    let countries = await getCountryByName('Germa');
+    console.log(countries);
+    for (country of countries){
+        countriesHtml.innerHTML += `
+        <articles class="card">
+            <picture class="card__flag"><img src="${country.flag}"></picture>
+            <h2 class="card__title">${country.name}</h2>
+            <h3 class="card__population-header">Population:<span class="card__population-content">${country.population}</span> </h3>
+            <h3 class="card__region-header">Region:<span class="card__region-content">${country.region}</span> </h3>
+            <h3 class="card__capital-header">Capital:<span class="card__capital-content">${country.capital}</span> </h3>
+        </articles>`
+    }
+}
+fillDatas();
+
