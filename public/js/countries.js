75021c93dbacc47a173b6b439c0394fa12f6e70e
Fabien V <fabien.vermeiren@gmail.com>
2022-03-18 16:56:37 +0100

    style country card

--- public/js/countries.js
+++ public/js/countries.js
@@ -10,12 +10,12 @@ const getCountryByName = async (name)=>{
 }
 const countriesHtml = document.getElementById('countries');
 const fillDatas = async ()=>{
-    let countries = await getCountryByName('Germa');
+    let countries = await getCountryByName('Be');
     console.log(countries);
     for (country of countries){
         countriesHtml.innerHTML += `
         <articles class="card">
-            <picture class="card__flag"><img src="${country.flag}"></picture>
+            <picture class="card__flag"><img class="card__img" src="${country.flag}" alt="${country.name} flag"></picture>
             <h2 class="card__title">${country.name}</h2>
             <h3 class="card__population-header">Population:<span class="card__population-content">${country.population}</span> </h3>
             <h3 class="card__region-header">Region:<span class="card__region-content">${country.region}</span> </h3>
