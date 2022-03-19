233a0dcab26129c58ff616d319f230a3086608a6
Fabien V <fabien.vermeiren@gmail.com>
2022-03-19 14:29:42 +0100

    add filtering by region

--- public/js/countries.js
+++ public/js/countries.js
@@ -1,19 +1,26 @@
 const getAllCountries = async ()=>{
     const reponse = await fetch('https://restcountries.com/v2/all');
     const datas = await reponse.json();
-    console.log(datas);
+    return datas
 }
 const getCountryByName = async (name)=>{
     const reponse = await fetch(`https://restcountries.com/v2/name/${name}`);
     const datas = await reponse.json();
     return datas;
 }
-const countriesHtml = document.getElementById('countries');
-const fillDatas = async (name)=>{
-    let countries = await getCountryByName(name);
-    console.log(countries);
+
+const fillDatas = async (name='All',region='')=>{
+    let countries;
+    if (name=='All' || name==''){
+        countries = await getAllCountries();
+    }else{
+        countries = await getCountryByName(name);
+    }
+    // console.log(countries);
+    countriesHtml.innerHTML = '';
     for (country of countries){
-        countriesHtml.innerHTML += `
+        if (region!=='' && region==country.region){
+            countriesHtml.innerHTML += `
         <articles class="card">
             <picture class="card__flag"><img class="card__img" src="${country.flag}" alt="${country.name} flag"></picture>
             <h2 class="card__title">${country.name}</h2>
@@ -21,14 +28,37 @@ const fillDatas = async (name)=>{
             <h3 class="card__region-header">Region:<span class="card__region-content">${country.region}</span> </h3>
             <h3 class="card__capital-header">Capital:<span class="card__capital-content">${country.capital}</span> </h3>
         </articles>`
+        }
+        if (region==''){
+            countriesHtml.innerHTML += `
+        <articles class="card">
+            <picture class="card__flag"><img class="card__img" src="${country.flag}" alt="${country.name} flag"></picture>
+            <h2 class="card__title">${country.name}</h2>
+            <h3 class="card__population-header">Population:<span class="card__population-content">${country.population}</span> </h3>
+            <h3 class="card__region-header">Region:<span class="card__region-content">${country.region}</span> </h3>
+            <h3 class="card__capital-header">Capital:<span class="card__capital-content">${country.capital}</span> </h3>
+        </articles>`
+        }
     }
 }
-
+const countriesHtml = document.getElementById('countries');
 const search = document.getElementById('submit-search');
 const searchInput=document.getElementById('search-input');
-
-search.addEventListener('click',async ()=>{
-    countriesHtml.innerHTML = '';
-    await fillDatas(searchInput.value);
+searchInput.addEventListener('keyup',async ()=>{
+    let region = document.getElementById('dropdown').textContent;
+    console.log(region);
+    if (region=='Filter by Region'){
+        console.log('ok')
+        await fillDatas(searchInput.value);
+    }else{
+        await fillDatas(searchInput.value,region);
+    } 
 })
+window.onload = async()=>{
+    await fillDatas();
+}
+
+
+
+
 
--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,9 +1,27 @@
+let content = document.getElementById('dropdown-region');
 document.getElementById('dropdown').addEventListener('click',()=>{
-    let content = document.getElementById('dropdown-region');
     if (content.style.display=='flex'){
         content.style.display='none';
     }else{
         content.style.display='flex';
     }
-    
+})
+let items = document.querySelectorAll('.dropdown-region__item');
+// console.log(items);
+items.forEach(item=>{
+    item.addEventListener('click',async()=>{
+        // console.log(item.textContent)
+        if (content.style.display=='flex'){
+            content.style.display='none';
+            document.getElementById('dropdown').textContent=`${item.textContent}`
+            if(searchInput.value ==''){
+                await fillDatas('All',item.textContent);
+            }else{
+                await fillDatas(searchInput.value,item.textContent);
+            }
+            
+        }else{
+            content.style.display='flex';
+        }
+    })
 })
